2cb89ed8917e2ca41089b98a72939907b245591c
Vedran <vedrandeskovski5@gmail.com>
2026-01-28 11:24:43 +0100

    ignore eslint

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Ignore ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = nextConfig;
