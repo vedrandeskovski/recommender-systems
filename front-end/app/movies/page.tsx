85f229a048428da0aa9a5854e3b3cbb7fe0ef8ae
Vedran <vedrandeskovski5@gmail.com>
2026-01-28 11:16:39 +0100

    dynamic render on movies

--- front-end/app/movies/page.tsx
+++ front-end/app/movies/page.tsx
@@ -1,6 +1,7 @@
+export const dynamic = "force-dynamic";
+
 import MoviesClient from "@/components/movies/moviesLayout";
 import { getMovies } from "@/services/movies";
-import { Spinner } from "@heroui/spinner";
 
 const MoviesPage = async () => {
   const data = await getMovies(null, "asc", 1, 10);
