2960c4cf0bc38ce0fb6338a5e5aa6bad240bfb3f
Vedran <vedrandeskovski5@gmail.com>
2026-01-30 13:18:07 +0100

    permitting vercel

--- back-end/app.py
+++ back-end/app.py
@@ -32,7 +32,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://recommender-systems.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
